3b6cb14f1826436e695bfc124cdbc27c1d8a7a08
Unknown <locphamduc96@gmail.com>
2018-07-24 01:39:31 +0700

    update tiep

--- algorithm.js
+++ algorithm.js
@@ -21,7 +21,7 @@ const BubbleSort = (arr) => {
     let arrCp = arr.slice()
     let n = arr.length
     for (let i = 0; i < n; i++) {
-        for (let j = i; j < n; j++) {
+        for (let j = 0; j < n; j++) {
             if (arrCp[j] > arrCp[j + 1]) {
                 let temp = arrCp[j]
                 arrCp[j] = arrCp[j + 1]
@@ -33,6 +33,152 @@ const BubbleSort = (arr) => {
     return arrCp
 }
 
+const InsertionSort = (arr) => {
+    
+    let arrCp = arr.slice() 
+    let n = arr.length
+
+    for (let i = 1; i < n; i++) {
+        
+        let value = arrCp[i]
+        let position = i
+
+        while (position > 0 && arrCp[position - 1]  > value) {
+            arrCp[position] = arrCp[position - 1]
+            position = position - 1
+        }
+
+        arrCp[position] = value
+    }
+
+    return arrCp
+}
+
+const MergeSort = (arr) => {
+
+    let arrLength = arr.length
+
+    if (arrLength === 1) {
+        return arr
+    }
+
+    const arr1 = arr.slice(0, arrLength/2)
+    const arr2 = arr.slice(arrLength/2, arrLength)
+
+    //console.log(arr1, arr2)
+
+    return Merge(MergeSort(arr1), MergeSort(arr2))
+}
+
+const Merge = (arr1, arr2) => {
+
+    let arr = []
+
+    while (arr1.length > 0 && arr2.length > 0) {
+
+        if (arr1[0] > arr2[0]) {
+            arr.push(arr2[0])
+            arr2.splice(0, 1)
+        } else {
+            arr.push(arr1[0])
+            arr1.splice(0, 1)
+        }
+    }
+
+    while (arr1.length > 0) {
+        arr.push(arr1[0])
+        arr1.splice(0, 1)
+    }
+
+    while (arr2.length > 0) {
+        arr.push(arr2[0])
+        arr2.splice(0, 1)
+    }
+
+    return arr
+}
+
+const ShellSort = (arr) => {
+
+    let arrCp = arr.slice()
+    let n = arrCp.length
+    let interval = 1
+
+    while (interval < Math.round(n / 3)) {
+        interval = interval*3 + 1
+    }
+
+    console.log(interval)
+
+    while (interval > 0) {
+
+        for (let outer = interval; outer < n; outer++) {
+
+            let valueToInsert = arrCp[outer]
+            let inner = outer
+
+            while (inner > interval - 1 && arrCp[inner - interval] >= valueToInsert) {
+                arrCp[inner] = arrCp[inner - interval]
+                inner = inner - interval
+            }
+
+            arrCp[inner] = valueToInsert
+        }
+
+        interval = Math.round((interval - 1) / 3)
+    }
+
+    return arrCp
+}
+
+var quickSort = function (array, left, right) {
+
+    let leftIndex = partition(array, left, right);
+
+    if (left < leftIndex - 1) {
+        quickSort(array, left, leftIndex - 1);
+    }
+
+    if (right > leftIndex) {
+        quickSort(array, leftIndex, right);
+    }
+
+    return array;
+}
+
+var swap = function (array, left, right) {
+    let temp;
+    temp = array[left];
+    array[left] = array[right];
+    array[right] = temp;
+}
+
+var partition = function (array, left, right) {
+    console.log(array)
+    let pivotIndex = Math.floor((left + right) / 2);
+    let pivot = array[pivotIndex];
+
+    let leftIndex = left;
+    let rightIndex = right;
+
+    while (leftIndex <= rightIndex) {
+        while (array[leftIndex] < pivot) {
+            leftIndex++;
+        }
+
+        while (array[rightIndex] > pivot) {
+            rightIndex--;
+        }
+
+        if (leftIndex <= rightIndex) {
+            swap(array, leftIndex, rightIndex);
+            leftIndex++;
+            rightIndex--;
+        }
+    }
+    return leftIndex;
+}
+
 const BinarySearch = (arr, findVal) => {
 
     let n = arr.length
@@ -42,8 +188,10 @@ const BinarySearch = (arr, findVal) => {
 
     while (true) {
 
-        let mid = Math.round(lower + (higher - lower)/2)
+        let mid = Math.floor(lower + (higher - lower)/2)
         if (arr[mid] == findVal) {
+            console.log(arr[mid])
+            console.log(mid)
             console.log("Tim thay")
             return true
         } 
@@ -63,9 +211,89 @@ const BinarySearch = (arr, findVal) => {
     }
 }
 
+const InterpolationSearch = (arr, findVal) => {
+
+    let n = arr.length
+    let lower = 0
+    let higher = n - 1
+    let found = false
+
+    while (true) {
+
+        if (lower > higher) {
+            console.log("Khong tim thay")
+            return false
+        }
+
+        let mid = Math.round(lower + ((higher - lower)/(arr[higher] - arr[lower]))*(findVal - arr[lower]))
+        console.log(lower, mid, higher)
+
+        if (arr[mid] < findVal) {
+            lower = mid + 1
+        }
+        if (arr[mid] > findVal) {
+            higher = mid - 1
+        }  
+
+        if (arr[mid] == findVal) {
+            console.log(arr[mid])
+            console.log(mid)
+            console.log("Tim thay")
+            return true
+        }
+    }
+}
+
+const Fibonacci = (n) => {
+    
+    // let a = 1, b = 0, temp
+
+    // while (n > 0) {
+
+    //     temp = a
+    //     a = a+b
+    //     b = temp
+    //     n--
+    // }
+
+    // return a
+
+    if (n <= 1) {
+        return 1
+    }
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2)
+}
+
+const HanoiTower = (disk, nguon, temp, dich) => {
+    if (disk == 1) {
+        console.log(nguon + " " + dich)
+    } else {
+        HanoiTower(disk-1, nguon, dich, temp)
+        HanoiTower(1, nguon, dich, temp)
+        HanoiTower(disk-1, dich, temp, nguon)
+    }
+}
+
+const Sum = (n) => {
+
+    if (n == 1)
+        return 1
+
+    return n + Sum(n-1)
+}
+
 module.exports = {
     PrintArray: PrintArray,
     LinearSearch: LinearSearch,
     BubbleSort: BubbleSort,
-    BinarySearch: BinarySearch
+    InsertionSort: InsertionSort,
+    MergeSort: MergeSort,
+    ShellSort: ShellSort,
+    quickSort: quickSort,
+    BinarySearch: BinarySearch,
+    Fibonacci: Fibonacci,
+    HanoiTower: HanoiTower,
+    Sum: Sum,
+    InterpolationSearch: InterpolationSearch
 }
--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 const algorithm = require('./algorithm')
 
-const array = [1, 3, 4, 7, 2, 5, 6, 10, 9 ,8]
-
-let findVal = 7
-
-// let kq = LinearSearch(array, findVal)
-
-// if (kq !== false) {
-//     console.log("Tim thay " + findVal + " tai vi tri: " + kq)
-// } else {
-//     console.log("Khong tim thay " + findVal + " trong mang! ")
+//let array = [1, 3, 4, 5, 2, 6, 8, 7, 9, 5 ,5 ,6 ,7]
+let array = [21, 13]
+let n = array.length
+// for (let i = 1; i <= 20; i++) {
+//     array.push(i*2)
 // }
+let findVal = 20
 
-let sortedArray = algorithm.BubbleSort(array)
-console.log(sortedArray)
-let kq = algorithm.BinarySearch(sortedArray, 8)
-console.log(kq)
+let temp = algorithm.Fibonacci(10)
+console.log(temp)
+
+// console.log("BinarySearch");
+// algorithm.BinarySearch(sortedArray, findVal)
+
+// console.log("InterpolationSearch");www
+// algorithm.InterpolationSearch(sortedArray, findVal);
